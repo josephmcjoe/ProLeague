542ea816054d3b20f899f3e53ca72d1caec46901
BenKanouse <kanobt61@gmail.com>
2015-03-03 14:49:55 -0500

    Add an index to teams
    Teams now have an index page where you can see all of the teams.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,8 +1,20 @@
 class TeamsController < ApplicationController
+  def create
+    Team.create(team_params)
+    redirect_to teams_path
+  end
+
+  def index
+    @teams = Team.all
+  end
+
   def new
     @team = Team.new
   end
 
-  def create
+  private
+
+  def team_params
+    params.require(:team).permit(:name)
   end
 end
--- spec/features/teams_spec.rb
+++ spec/features/teams_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe "Teams", type: :request do
       visit new_team_path
       expect(page).to have_content("New Team")
       expect(page).to have_content("Name")
+      page.fill_in "Name", :with => "CJ Entus"
+      click_button "Create Team"
+      expect(page).to have_content("All Teams")
+      expect(page).to have_content("Team Name")
+      expect(page).to have_content("CJ Entus")
     end
   end
 end
